5f804ffe80b2396eba6f3a87039d7dcca5ebd377
stalscar <Florescu132#>
2021-08-06 15:09:35 +0200

    added sensors for 1:n communication

--- Sensor/src/main/java/Main.java
+++ Sensor/src/main/java/Main.java
@@ -5,7 +5,6 @@ public class Main {
  */
     public static void main(String[] args) throws Exception{
 
-        System.out.println("TEST");
         String ip;
         int port;
         String location;
