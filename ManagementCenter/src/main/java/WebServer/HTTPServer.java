d3dc9cd0a64f43e114c3d38847444c0f5731162c
stdehilz <dennis.hilz@stud.h-da.de>
2021-08-09 15:56:37 +0200

    [code, fix] #2 Implemented simple switch case distinction for path enumeration -> json output is now being displayed in browser

--- ManagementCenter/src/main/java/WebServer/HTTPServer.java
+++ ManagementCenter/src/main/java/WebServer/HTTPServer.java
@@ -6,9 +6,18 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.lang.reflect.Array;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import com.google.gson.*;
 
 public class HTTPServer{
     private ServerSocket httpSocket;
@@ -42,41 +51,78 @@ public class HTTPServer{
         }
         if (request.isEmpty())
             return;
-        parseHTTPRequest(request);
-        sendClientResponse(client);
-        for (SensorData data : this.udpReceiver.getSensorData()) {
-            System.out.println(data);
-        }
+        String outputString = handleClientResponse(parseHTTPRequest(request));
+        sendClientResponse(client, outputString);
     }
 
-    private void parseHTTPRequest(ArrayList<String> request) {
+    private Map<String, String> parseHTTPRequest(ArrayList<String> request) {
+
+        Map<String, String> requestMap = new HashMap<String, String>();
         String[] requestLine = request.get(0).split(" ");
-        String method = requestLine[0];
-        String path = requestLine[1];
-        String version = requestLine[2];
-        String host = request.get(1).split(" ")[1];
+
+        requestMap.put("method", requestLine[0]);
+        requestMap.put("path", requestLine[1]);
+        requestMap.put("version", requestLine[2]);
+        requestMap.put("host", request.get(1).split(" ")[1]);
 
         StringBuilder requestLogEntry = new StringBuilder();
         requestLogEntry.append("[HTTPServer] ");
-        requestLogEntry.append(method + " | ");
-        requestLogEntry.append(path + " | ");
-        requestLogEntry.append(version+ " | ");
-        requestLogEntry.append(host + " | ");
+        requestLogEntry.append(requestMap.get("method") + " | ");
+        requestLogEntry.append(requestMap.get("path") + " | ");
+        requestLogEntry.append(requestMap.get("version") + " | ");
+        requestLogEntry.append(requestMap.get("host") + " | ");
         System.out.println(requestLogEntry);
+
+        return requestMap;
     }
 
-    private void handleClientResponse(Socket client, String path) throws IOException {
-
+    private String getAll(){
+        return new GsonBuilder().setPrettyPrinting().create().toJson(this.udpReceiver.getSensorData());
     }
 
-    private static void sendClientResponse(Socket client) throws IOException {
+    private String getHistory(String location){
+        List<SensorData> filteredSensorData = this.udpReceiver.getSensorData().stream().filter(
+                sensorData -> sensorData.getLocation().equals(location)).collect(Collectors.toList());
+        return new GsonBuilder().setPrettyPrinting().create().toJson(filteredSensorData);
+    }
+    private String getCurrent(String location){
+        List<SensorData> filteredSensorData = this.udpReceiver.getSensorData().stream().filter(
+                sensorData -> sensorData.getLocation().equals(location)).collect(Collectors.toList());
+        SensorData currentData = filteredSensorData.get(filteredSensorData.size()-1);
+        return new GsonBuilder().setPrettyPrinting().create().toJson(currentData);
+    }
+
+    private String handleClientResponse(Map<String, String> requestMap) throws IOException {
+        String jsonOutput;
+        String path = requestMap.get("path");
+        System.out.println(path);
+        String[] attributes = path.split("/");
+        switch (attributes[1]) {
+            case "": {
+                jsonOutput = getAll();
+                break;
+            }
+            case "history": {
+                jsonOutput = getHistory(attributes[2]);
+                break;
+            }
+            case "current":
+                jsonOutput = getCurrent(attributes[2]);
+                break;
+            default:
+                jsonOutput = "Bad request";
+        }
+    return jsonOutput;
+    }
+
+    private static void sendClientResponse(Socket client, String output) throws IOException {
         // TODO: Send different responses according to request. No POST Requests or invalid Requests
         // TODO: Switch case for different paths and function calls to retrieve the respective sensorData
         OutputStream clientOutput = client.getOutputStream();
         clientOutput.write("HTTP/1.1 200 OK\r\n".getBytes());
-        clientOutput.write(("ContentType: text/html\r\n").getBytes());
+        clientOutput.write(("ContentType: application/json\r\n").getBytes());
         clientOutput.write("\r\n".getBytes());
-        clientOutput.write("<b>Hell Yeah!</b>".getBytes());
+        clientOutput.write(output.getBytes());
         clientOutput.write("\r\n\r\n".getBytes());
         clientOutput.flush();
         client.close();
